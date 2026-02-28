da81a615b18890f075ceafb0299557dd4f110ac7
p-porciello <pporciello@ucvts.org>
2026-02-28 02:54:37 -0500

    made admin page visible in navbar if user 's role is "admin"

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { pageData } from './pageData';
+import { pageData, adminPageData } from './pageData';
+import { User } from '@/types';
+import { jwtDecode } from 'jwt-decode';
 
 export function Navbar() {
+  const [data, setData] = useState<{name: string, path: string}[]>([]);
+
+  useEffect(() => {
+      async function loadUserData() {
+        const token = sessionStorage.getItem('User');
+        if (!token) return;
+        const decodedUser = jwtDecode<User>(token);
+        if (decodedUser.role === "admin") {
+          setData(adminPageData);
+        } else {
+          setData(pageData);
+        }
+      }
+      loadUserData();
+    }, []);
+
   const navigate = useNavigate();
 
   function handleLogout() {
@@ -9,13 +28,13 @@ export function Navbar() {
     navigate('/');
   }
 
-  return (
+    return (
     <div className="navbar">
-      {pageData.map((page) => {
+      {data.map((page) => {
         return (
           <Link to={page.path} className="navItem" key={page.name}>
             <button>{page.name}</button>
-          </Link>
+          </Link> 
         );
       })}
       <button onClick={handleLogout}>Log Out</button>
--- client/src/components/pageData.ts
+++ client/src/components/pageData.ts
@@ -28,3 +28,38 @@ export const pageData = [
     path: '/profile',
   },
 ];
+
+export const adminPageData = [
+  {
+    name: 'Home',
+    path: '/home',
+  },
+  {
+    name: 'Admin',
+    path: '/admin'
+  },
+  {
+    name: 'Lost & Found',
+    path: '/lost-and-found',
+  },
+  {
+    name: 'Submit an Item',
+    path: '/submit-item',
+  },
+  {
+    name: 'UCVTS Map',
+    path: '/map',
+  },
+  {
+    name: 'About Us',
+    path: '/about-us',
+  },
+  {
+    name: 'FAQ',
+    path: '/faq',
+  },
+  {
+    name: 'Your Profile',
+    path: '/profile',
+  },
+];
