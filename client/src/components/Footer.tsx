db26d50a44ff06efdfdb0d57ee7e01f1cd742ca6
p-porciello <pporciello@ucvts.org>
2026-03-09 21:03:58 -0400

    fixed tags colors + footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,7 +2,8 @@ export function Footer() {
     return (
     <div className="footer" style={{display:'flex', flexDirection:'column', paddingBottom:'5vh', marginTop:'2vh',paddingTop:'2vh', backgroundColor:'var(--primary)', color:'white', width:'100%',bottom:'0'}}>
             <h2>UCVTS Lost and Found</h2>
-            <p> This Website was Developed by Paige Porciello and Caitlin Sayah. All icons from Font Awesome, images taken by Paige and Caitlin.</p>
+            <p>A website developed by Paige Porciello and Caitlin Sayah.</p>
+            <p>All icons from Font Awesome, all images taken by Paige and Caitlin.</p>
 
         </div>
     )
--- client/src/components/Settings.tsx
+++ client/src/components/Settings.tsx
@@ -15,12 +15,14 @@ export function Settings() {
       root.style.setProperty('--primary', "#1932A1");
       root.style.setProperty('--secondary', "#4D59FF");
       root.style.setProperty('--accent', "#2002BA");
+      root.style.setProperty('--tags-color', "#1b2170")
     } else {
       root.style.setProperty('--text-color', "#020114");
       root.style.setProperty('--primary', "#001448");
       root.style.setProperty('--secondary', "#7e87ff");
       root.style.setProperty('--accent', "#5058de");
       root.style.setProperty('--light-bg', '#fcfdff');
+      root.style.setProperty('--tags-color', "#c6c9ee")
     }
 
     setMode(darkMode);
