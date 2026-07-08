3dcd70bd5ebef58ce38db298813bd151ee453466
p-porciello <pporciello@ucvts.org>
2026-07-08 17:39:44 -0400

    did most of styling for lost & found catalogue page (still need to add functionality)

--- client/src/pages/lost-and-found.tsx
+++ client/src/pages/lost-and-found.tsx
@@ -52,25 +52,66 @@ export function LostAndFound() {
 
   return (
     <>
-      <h1>All Lost Items</h1>
+      <div id="catalogue-content">
 
-      <div className="search">
-        <i className="fas fa-search"></i>
-        <input id="searchBar" aria-label="Searches for items based on name"
-          type="text"
-          placeholder="Search for an item..."
-          value={searchTerm}
-          onChange={handleSearch}
-          style ={{ backgroundImage: 'src/assets/search-ui-icon.png.webp', backgroundPosition: 'right', width:'50%'}}
-        />
+        <div id="filter-bar">
+          <h2 className="no-bg">Filters</h2>
+          <div className="filter-menu">
+            <button className="filter-header">Color</button>
+            <div id="colors-menu" className="ops-menu">
+              <label><input type="checkbox" className="filter-option"/> Red</label>
+              <label><input type="checkbox" className="filter-option"/> Orange</label>
+              <label><input type="checkbox" className="filter-option"/> Yellow</label>
+              <label><input type="checkbox" className="filter-option"/> Green</label>
+              <label><input type="checkbox" className="filter-option"/> Blue</label>
+              <label><input type="checkbox" className="filter-option"/> Purple</label>
+              <label><input type="checkbox" className="filter-option"/> Black</label>
+              <label><input type="checkbox" className="filter-option"/> White</label>
+              <label><input type="checkbox" className="filter-option"/> Gray</label>
+              <label><input type="checkbox" className="filter-option"/> Beige/Tan</label>
+            </div>
+          </div>
+          <div className="filter-menu">
+            <button className="filter-header" >Type</button>
+              <div id="types-menu" className="ops-menu">
+                <label><input type="checkbox" className="filter-option"/> Water bottle</label>
+                <label><input type="checkbox" className="filter-option"/> Clothing</label>
+                <label><input type="checkbox" className="filter-option"/> Technology</label>
+                <label><input type="checkbox" className="filter-option"/> Accessory</label>
+                <label><input type="checkbox" className="filter-option"/> Pencil/Pen</label>
+            </div>
+          </div>
+          <div className="filter-menu">
+            <button className="filter-header">Brand</button>
+          </div>
+        </div>
+
+        <div>
+            <div id="search-container">
+              <h1 className="no-bg">All Lost Items</h1>
+            
+              <div className="search">
+                <i className="fas fa-search"></i>
+                <input id="searchBar" aria-label="Searches for items based on name"
+                  type="text"
+                  placeholder="Search for an item..."
+                  value={searchTerm}
+                  onChange={handleSearch}
+                  style ={{ backgroundImage: 'src/assets/search-ui-icon.png.webp', backgroundPosition: 'right', width:'50%'}}
+                />
+              </div>
+              { searchTerm ? <p>Showing <b>{items.length}</b> results for <b>"{searchTerm}"</b></p> : <p></p>}
+              <p><b>Applied Filters: </b></p>
+            </div>
+          
+          <MasonryLayout>
+            {items.map((item, index) => {
+              return <CatalogueCard item={item} key={index}/>
+            })}
+          </MasonryLayout>
+        </div>
+
       </div>
-
-        <MasonryLayout>
-          {items.map((item, index) => {
-            return <CatalogueCard item={item} key={index}/>
-          })}
-        </MasonryLayout>
-      
     </>
   );
 }
